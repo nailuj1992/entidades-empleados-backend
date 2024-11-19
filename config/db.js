45cf6570748a7bd5c5c2a5b17922350d37e40494
Julian Gonzalez Prieto <juliangonzalezprieto@hotmail.com>
2024-11-19 17:33:30 -0500

    mongodb

--- config/db.js
+++ config/db.js
@@ -5,7 +5,9 @@ const connectDB = async () => {
     await mongoose.connect(process.env.MONGO_URI, {
       useNewUrlParser: true,
       useUnifiedTopology: true,
-    });
+    })
+      .then(() => console.log("Conectado a MongoDB Atlas"))
+      .catch((err) => console.error("Error al conectar a MongoDB", err));
     console.log("MongoDB conectado...");
   } catch (error) {
     console.error(error.message);
