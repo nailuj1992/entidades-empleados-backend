3b59b8aebeff6118bf008afef3cd5f50b47d6596
Julian Gonzalez Prieto <juliangonzalezprieto@hotmail.com>
2024-11-19 17:54:52 -0500

    server

--- server.js
+++ server.js
@@ -17,4 +17,4 @@ app.use("/api/auth", authRoutes);
 
 //const PORT = process.env.PORT || 5000;
 //app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
-module.exports = createServer(app);
+module.exports = app;
