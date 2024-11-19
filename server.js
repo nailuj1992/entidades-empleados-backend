ce4f51f0beeb4199d740a59412185cd99e7326fb
Julian Gonzalez Prieto <juliangonzalezprieto@hotmail.com>
2024-11-19 17:39:07 -0500

    vercel

--- server.js
+++ server.js
@@ -5,11 +5,16 @@ const authRoutes = require("./routes/authRoutes");
 const entidadRoutes = require("./routes/entidadRoutes");
 
 const app = express();
+const { createServer } = require("@vercel/node");
 connectDB();
 
 app.use(express.json());
+app.get("/", (req, res) => {
+    res.send("API funcionando en Vercel");
+});
 app.use("/api/entidades", entidadRoutes);
 app.use("/api/auth", authRoutes);
 
-const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+//const PORT = process.env.PORT || 5000;
+//app.listen(PORT, () => console.log(`Servidor corriendo en puerto ${PORT}`));
+module.exports = createServer(app);
