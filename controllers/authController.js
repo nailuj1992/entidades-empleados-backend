f83c1ccc50e98ad89935df695e03dd49fb914e23
Julian Gonzalez Prieto <juliangonzalezprieto@hotmail.com>
2024-11-20 01:07:14 -0500

    try catch auth

--- controllers/authController.js
+++ controllers/authController.js
@@ -3,27 +3,35 @@ const jwt = require("jsonwebtoken");
 let bcrypt = require('bcryptjs');
 
 exports.login = async (req, res) => {
-  const { username, password } = req.body;
+  try {
+    const { username, password } = req.body;
 
-  const user = await User.findOne({ username });
-  if (!user || !(await bcrypt.compare(password, user.password))) {
-    return res.status(401).json({ message: "Credenciales inválidas" });
+    const user = await User.findOne({ username });
+    if (!user || !(await bcrypt.compare(password, user.password))) {
+      return res.status(401).json({ message: "Credenciales inválidas" });
+    }
+
+    const token = jwt.sign(
+      { id: user._id, role: user.role },
+      process.env.JWT_SECRET,
+      { algorithm: "HS256", expiresIn: process.env.JWT_EXPIRATION }
+    );
+
+    res.json({ token });
+  } catch (error) {
+    res.status(500).json({ message: "Error al intentar logear", error });
   }
-
-  const token = jwt.sign(
-    { id: user._id, role: user.role },
-    process.env.JWT_SECRET,
-    { algorithm: "HS256", expiresIn: process.env.JWT_EXPIRATION }
-  );
-
-  res.json({ token });
 };
 
 exports.register = async (req, res) => {
-  const { username, password, role } = req.body;
+  try {
+    const { username, password, role } = req.body;
 
-  const user = new User({ username, password, role });
-  await user.save();
+    const user = new User({ username, password, role });
+    await user.save();
 
-  res.status(201).json({ message: "Usuario creado" });
+    res.status(201).json({ message: "Usuario creado" });
+  } catch (error) {
+    res.status(500).json({ message: "Error al crear usuario", error });
+  }
 };
