0842d6f23e3e6f2bf9e2557d1d23a912b918a535
Julian Gonzalez Prieto <juliangonzalezprieto@hotmail.com>
2024-11-19 21:29:48 -0500

    habilitar cors

--- server.js
+++ server.js
@@ -1,11 +1,15 @@
 require("dotenv").config();
 const express = require("express");
+const cors = require('cors');
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const entidadRoutes = require("./routes/entidadRoutes");
 
 const app = express();
 connectDB();
+
+// Habilitar CORS para todas las solicitudes
+app.use(cors());  // Esto permite CORS para todos los orígenes
 
 app.use(express.json());
 app.get("/", (req, res) => {
